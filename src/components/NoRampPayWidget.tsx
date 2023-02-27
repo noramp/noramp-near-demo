5cb4fad0cd9bb480c889d1e4e11b68a484ef663c
Eli José Carrasquero <ielijose@gmail.com>
2023-02-26 23:32:28 -0600

    add auth

--- src/components/NoRampPayWidget.tsx
+++ src/components/NoRampPayWidget.tsx
@@ -13,7 +13,13 @@ const NoRampPayWidget = ({ price }) => {
 
       if (event.data.event === 'noramp:onPayment') {
         console.log('Payment event', event.data);
-        setSuccess(event.data?.detail?.type === 'finished');
+
+        const type = event.data?.detail?.type;
+        const status = event.data?.detail?.data?.status;
+
+        if (type === 'finished' && status === 'paid') {
+          setSuccess(true);
+        }
       }
     });
   }, []);
@@ -23,7 +29,7 @@ const NoRampPayWidget = ({ price }) => {
   return (
     <div className="flex flex-col items-center">
       <iframe
-        src={`${NORAMP_EMBED_URL}/embed/payments/${NORAMP_APP_ID}?device=desktop&theme=dark&price_id=${price.id}`}
+        src={`${NORAMP_EMBED_URL}/embed/payments/${NORAMP_APP_ID}?device=desktop&theme=dark&price_id=${price.id}&auth=true`}
         height="180"
         width="450"
       />
